b485248ae93d372054c037327fda8ca4e89ba58a
zoltanJakubecz <jakuzo@gmail.com>
2020-05-26 13:24:25 +0200

    Initialize recommendation service

---
+++ recommendation-service/src/main/java/com/jakuza/recommendationservice/model/Recommendation.java
@@ -0,0 +1,29 @@
+package com.jakuza.recommendationservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Recommendation {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private int rating;
+
+    private String comment;
+
+    private Long videoId;
+
+}
